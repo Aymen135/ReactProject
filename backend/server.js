3d14c53a797ac9d639cce8fd490e88726e418f99
Aymen <aymenbensaid806@gmail.com>
2023-06-01 10:35:04 +0100

    backend

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from 'dotenv';
+import { notFound, errorHander } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
+
+
+dotenv.config();
+const port = process.env.PORT || 5000;
+import userRoutes from './routes/userRoutes.js';
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use('/api/users', userRoutes);
+app.get('/', (req, res) => res.send('Server is ready'));
+app.use(notFound);
+app.use(errorHander);
+app.listen(port, () => console.log(`Server started on port ${port}`));
